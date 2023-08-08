4db699debbe420da37ab3bb854345ffa85b666bf
vladoja <dr.jalo2@gmail.com>
2023-08-08 14:09:05 +0200

    userProvider

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,16 +1,21 @@
 import 'dart:convert';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart';
+import 'package:riverpod_fetch_api/models/user_model.dart';
 
 class APIServices {
   String endpoint = 'https://reqres.in/api/users?page=2';
 
-  getUsers() async {
+  Future<List<UserModel>> getUsers() async {
     Response response = await get(Uri.parse(endpoint));
     if (response.statusCode == 200) {
       final List result = jsonDecode(response.body)['data'];
+      return result.map((e) => UserModel.fromJson(e)).toList();
     } else {
       throw Exception(response.reasonPhrase);
     }
   }
 }
+
+final userProvider = Provider<APIServices>((ref) => APIServices());
