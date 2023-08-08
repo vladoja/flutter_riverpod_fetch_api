f8e06a2b14b4a97e9f83f2c372ad11dd7e4c457b
vladoja <dr.jalo2@gmail.com>
2023-08-08 17:19:58 +0200

    Page body is showing AsyncValue

--- lib/pages/myhome_page.dart
+++ lib/pages/myhome_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_fetch_api/providers/data_provider.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _data = ref.watch(userDataProvider);
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -12,7 +15,14 @@ class MyHomePage extends StatelessWidget {
         title: const Text("Riverpod Fetch API", textAlign: TextAlign.center),
         centerTitle: true,
       ),
-      body: const Center(child: Text("Fetch API")),
+      body: _data.when(
+          data: (data) {
+            return Column(
+              children: [Text('Total: ${data.length}')],
+            );
+          },
+          error: (error, stackTrace) => Text(error.toString()),
+          loading: () => const Center(child: CircularProgressIndicator())),
     );
   }
 }
