b6c88bb86922e40f4cbcb3656c3ca6475fbdeba7
vladoja <dr.jalo2@gmail.com>
2023-08-08 17:31:48 +0200

    List of User Cards

--- lib/pages/myhome_page.dart
+++ lib/pages/myhome_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_fetch_api/models/user_model.dart';
 import 'package:riverpod_fetch_api/providers/data_provider.dart';
 
 class MyHomePage extends ConsumerWidget {
@@ -17,8 +18,42 @@ class MyHomePage extends ConsumerWidget {
       ),
       body: _data.when(
           data: (data) {
-            return Column(
-              children: [Text('Total: ${data.length}')],
+            List<UserModel> userList = data
+                .map(
+                  (e) => e,
+                )
+                .toList();
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: userList.length,
+                      itemBuilder: (_, index) {
+                        return Card(
+                          color: Colors.blue,
+                          elevation: 4,
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          child: ListTile(
+                            title: Text(
+                              userList[index].firstName!,
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                            subtitle: Text(
+                              userList[index].lastName!,
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                            trailing: CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage(userList[index].avatar!)),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             );
           },
           error: (error, stackTrace) => Text(error.toString()),
